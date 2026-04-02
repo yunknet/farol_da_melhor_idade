43d51dece47d24bb41cb6ac19e48bf025f7cb3b7
yunknet <n3t.soares@gmail.com>
2026-04-02 11:07:04 -0300

    chore: limpa arquivos da ynk e roilead

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,25 +5,14 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import UpdatePassword from "./pages/UpdatePassword";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfUse from "./pages/TermsOfUse";
-import CompleteRegistration from "./pages/CompleteRegistration";
-import Subscription from "./pages/Subscription";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "./components/ScrollToTop";
-import DemoCursor from "./pages/DemoCursor";
-import DemoSchema from "./pages/DemoSchema";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
@@ -32,17 +20,6 @@ const App = () => (
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsOfUse />} />
-            <Route path="/complete-registration" element={<CompleteRegistration />} />
-            <Route path="/subscription" element={<Subscription />} />
-            <Route path="/demo-cursor" element={<DemoCursor />} />
-            <Route path="/demo-schema" element={<DemoSchema />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
--- src/components/demo.tsx
+++
@@ -1,9 +0,0 @@
-import SchemaCard from "@/components/ui/schema-card-with-animated-wave-visualizer";
-
-const DemoOne = () => {
-    return (
-        <SchemaCard />
-    );
-};
-
-export { DemoOne };
